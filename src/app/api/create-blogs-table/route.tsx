da83adb0ef569280df71b2f48819f1003ce27459
pablo <pablo.puac@dooply.io>
2024-05-23 10:45:03 -0600

    creating blogs

--- src/app/api/create-blogs-table/route.tsx
+++ src/app/api/create-blogs-table/route.tsx
@@ -3,8 +3,7 @@ import { NextResponse } from 'next/server';
  
 export async function GET(request: Request) {
   try {
-    const result =
-      await sql`CREATE TABLE Blogs (Name varchar(128),Summary varchar(255),Content TEXT);`;
+    const result = await sql`CREATE TABLE Blogs (Name varchar(128),Summary varchar(255),Content TEXT);`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
